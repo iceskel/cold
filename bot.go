3818b511db40b55c6ab24dc17dbeef8f17c26800
co mz <ji@.com>
2014-07-11 23:51:27 +0800

    add repeat messenger and tweet command

--- bot.go
+++ bot.go
@@ -2,27 +2,34 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
+	"github.com/ChimeraCoder/anaconda"
+	"github.com/iceskel/lastfm"
 	"github.com/thoj/go-ircevent"
 	"io/ioutil"
 	"log"
 	"math/rand"
 	"strconv"
 	"time"
-	"flag"
-	"github.com/iceskel/lastfm"
 )
 
 type Configuration struct {
-	Channels     []string
-	Botname      string
-	Aouth        string
-	LastfmKey    string
-	LastfmSecret string
-	LastfmUser	 string
+	Channel               string
+	Botname               string
+	Aouth                 string
+	LastfmKey             string
+	LastfmSecret          string
+	LastfmUser            string
+	RepeatMsg             string
+	TwitterConsumerKey    string
+	TwitterConsumerSecret string
+	TwitterAccessToken    string
+	TwitterAccessSecret   string
 }
 
 var (
 	config Configuration
+	tweet  *anaconda.TwitterApi
 )
 
 func main() {
@@ -38,18 +45,19 @@ func main() {
 		log.Fatal(err)
 	}
 
+	anaconda.SetConsumerKey(config.TwitterConsumerKey)
+	anaconda.SetConsumerSecret(config.TwitterConsumerSecret)
+	tweet = anaconda.NewTwitterApi(config.TwitterAccessToken, config.TwitterAccessSecret)
+
 	con := irc.IRC(config.Botname, config.Botname)
 	con.Password = config.Aouth
 	if err := con.Connect("irc.twitch.tv:6667"); err != nil {
 		log.Fatal(err)
 	}
 
-	for _, channel := range config.Channels {
-		JoinChannel(channel, con)
-	}
+	JoinChannel(config.Channel, con)
 	con.Loop()
 }
-
 
 func JoinChannel(channel string, con *irc.Connection) {
 	con.AddCallback("001", func(e *irc.Event) {
@@ -57,6 +65,33 @@ func JoinChannel(channel string, con *irc.Connection) {
 		log.Print("Joined " + channel + " \n")
 		RollCommand(channel, con)
 		SongCommand(channel, con)
+		TweetCommand(channel, con)
+		RepeatMessenger(channel, con)
+	})
+}
+
+func RepeatMessenger(channel string, con *irc.Connection) {
+	ticker := time.NewTicker(time.Minute * 5)
+	for {
+		<-ticker.C
+		con.Privmsg(channel, config.RepeatMsg)
+	}
+}
+
+func TweetCommand(channel string, con *irc.Connection) {
+	delay := time.Now()
+	con.AddCallback("PRIVMSG", func(e *irc.Event) {
+		if e.Arguments[0] == channel {
+			if len(e.Message()) == 6 && time.Since(delay).Seconds() > 10 {
+				if e.Message()[0:6] == "!tweet" {
+					thetweet, err := tweet.GetUserTimeline(nil)
+					if err != nil {
+						log.Fatal(err)
+					}
+					con.Privmsg(channel, thetweet[0].CreatedAt+": \""+thetweet[0].Text+"\"")
+				}
+			}
+		}
 	})
 }
 
