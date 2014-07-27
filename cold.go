0ee455a75a5e909017544a63d7e058b8c7e18107
f <fhakani@gmail.com>
2014-07-27 18:11:34 +0800

    add command to control foobar2k

--- cold.go
+++ cold.go
@@ -4,14 +4,16 @@ import (
 	"encoding/json"
 	"flag"
 	"github.com/ChimeraCoder/anaconda"
-	"github.com/aybabtme/rgbterm"
 	"github.com/iceskel/lastfm"
+	"github.com/lxn/win"
 	"github.com/thoj/go-ircevent"
 	"io/ioutil"
 	"log"
 	"math/rand"
 	"strconv"
+	"syscall"
 	"time"
+	"unsafe"
 )
 
 type configuration struct {
@@ -28,12 +30,17 @@ type configuration struct {
 	TwitterAccessSecret   string
 }
 
+const (
+	vkA = 0x41 // win32 virtual key A code
+	vkX = 0x42 // win32 virtual key B code
+)
+
 var (
 	config      configuration
 	tweet       *anaconda.TwitterApi
-	fm          *lastfm.Lastfm
 	timeoutList = make(map[string]bool)
 	opList      = make(map[string]bool)
+	hwnd        win.HWND
 )
 
 func main() {
@@ -49,13 +56,16 @@ func main() {
 		log.Fatal(err)
 	}
 
+	foobar2kwindowclass := syscall.StringToUTF16Ptr("{97E27FAA-C0B3-4b8e-A693-ED7881E99FC1}")
+	foobar2kwindowname := syscall.StringToUTF16Ptr("foobar2000 v1.2.9")
+	hwnd = win.FindWindow(foobar2kwindowclass, foobar2kwindowname)
+	if unsafe.Pointer(hwnd) == nil {
+		log.Fatal("Foobar2k not open or not in default state (press the stop button)")
+	}
+
 	anaconda.SetConsumerKey(config.TwitterConsumerKey)
 	anaconda.SetConsumerSecret(config.TwitterConsumerSecret)
 	tweet = anaconda.NewTwitterApi(config.TwitterAccessToken, config.TwitterAccessSecret)
-	fm, err = lastfm.NewLastfm(config.LastfmUser, config.LastfmKey)
-	if err != nil {
-		log.Fatal(err)
-	}
 	opList[config.Channel[1:]] = true // op's for channel, gets op only commands
 	con := irc.IRC(config.Botname, config.Botname)
 	con.Password = config.Aouth
@@ -70,15 +80,13 @@ func main() {
 func joinChannel(channel string, con *irc.Connection) {
 	con.AddCallback("001", func(e *irc.Event) {
 		con.Join(channel)
-		var r, g, b uint8
-		r, g, b = 216, 52, 52
-		colored := rgbterm.String(channel, r, g, b)
-		log.Print("Joined " + colored)
+		log.Printf("Joined %s", channel)
 		songCommand(channel, con)
 		tweetCommand(channel, con)
 		timeoutCop(channel, 20, con)
 		addTimeoutList(channel, con)
 		rollCommand(channel, con)
+		foobar2kCommands(channel, 10, con)
 		repeatMessenger(channel, con) // must be last
 	})
 }
@@ -103,7 +111,8 @@ func tweetCommand(channel string, con *irc.Connection) {
 
 		thetweet, err := tweet.GetUserTimeline(nil)
 		if err != nil {
-			log.Fatal(err)
+			con.Privmsg(channel, "Tweet command not available, please try later")
+			return
 		}
 		con.Privmsgf(channel, "► %s: \"%s\"", thetweet[0].CreatedAt, thetweet[0].Text)
 		delay = time.Now()
@@ -138,14 +147,19 @@ func songCommand(channel string, con *irc.Connection) {
 		if e.Message() != "!song" && e.Message() != "!music" {
 			return
 		}
-
+		fm, err := lastfm.NewLastfm(config.LastfmUser, config.LastfmKey)
+		if err != nil {
+			con.Privmsg(channel, "Song command not available, please try later")
+			return
+		}
 		artist, trackName := fm.GetCurrentArtistAndTrackName()
 		if fm.IsNowPlaying() {
 			con.Privmsgf(channel, "► %s - %s", artist, trackName)
 		} else {
 			lastPlay, err := fm.GetLastPlayedDate()
 			if err != nil {
-				log.Fatal(err)
+				con.Privmsg(channel, "Song command not available, please try later")
+				return
 			}
 			con.Privmsgf(channel, "► %s - %s. Last played %s", artist, trackName, lastPlay)
 		}
@@ -174,6 +188,29 @@ func timeoutCop(channel string, length int, con *irc.Connection) {
 	con.AddCallback("PRIVMSG", func(e *irc.Event) {
 		if timeoutList[e.Message()] {
 			con.Privmsgf(channel, "/timeout %s %d", e.Nick, length)
+		}
+	})
+}
+
+func foobar2kCommands(channel string, limit int, con *irc.Connection) {
+	total := 0
+	con.AddCallback("PRIVMSG", func(e *irc.Event) {
+		if e.Message() == "!music next" || e.Message() == "!song next" {
+			total++
+			if !(total >= limit) {
+				return
+			}
+			win.PostMessage(hwnd, win.WM_KEYDOWN, vkX, 1)
+			win.PostMessage(hwnd, win.WM_KEYUP, vkX, 1)
+			total = 0
+		} else if e.Message() == "!music random" || e.Message() == "!song random" {
+			total++
+			if !(total >= limit) {
+				return
+			}
+			win.PostMessage(hwnd, win.WM_KEYDOWN, vkA, 1)
+			win.PostMessage(hwnd, win.WM_KEYUP, vkA, 1)
+			total = 0
 		}
 	})
 }
